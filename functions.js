c345764e32ea43efeea76bd551614bd9165d2a51
suelenk <suelenmatosr@gmail.com>
2022-09-25 19:47:50 -0400

    finished functions.js

--- functions.js
+++ functions.js
@@ -2,12 +2,55 @@
 
 // 1. isHometown
 
-// Define your function here
+function isHometown(town) {
+    // ""Given a `town`, return `True` if `town` is 'San Francisco'"""
+
+    if (town === "San Francisco") {
+        return true;   
+    } else {
+        return false;
+    }
+}
+
+const result = isHometown("Virginia")
+// console.log(result)
+
+
+
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(firstName, lastName) {
+// """Given `first_name` and `last_name`, return a full name."""
+
+    return `${firstName}${lastName} `;
+}
+// console.log(getFullName("Suelen ", "Karbivnychyy"))
+
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(basePrice, state, tax=0.05) {
+// """Return the total price of an item, figuring in state taxes and fees."""
+
+    const subtotal = basePrice * (1 + tax);
+    let fee = 0;
+
+    if ( state === "CA" ) {
+        fee = 0.03 * subtotal
+    } else if ( state === "PA" ) {
+        fee = 2        
+    } else if ( state === "MA") {
+        if (basePrice <= 100) {
+            fee = 1
+        } else {
+            fee = 3
+        }
+
+    }
+    return subtotal + fee
+}
+
+// console.log(calculateTotal(50.3, "PA"))
+
+
